ef8aa15a8931939075a9aefad3efe059ef78d3a0
sftimuritamb-creator <sf.timurita.mb@gmail.com>
2025-10-18 23:32:10 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,22 @@
-self.addEventListener('install', () => {
-  console.log('Service Worker installed.');
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('timurita-cache-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './Timurita_logo_192x192.png',
+        './Timurita_logo_512x512.png'
+      ]);
+    })
+  );
+  console.log('✅ Service Worker installed and cached files.');
 });
 
-self.addEventListener('fetch', () => {
-  // Online mode only – no caching
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
