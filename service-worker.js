4ded093f163c1741e919c3084cae4598b1f1e6da
sftimuritamb-creator <sf.timurita.mb@gmail.com>
2025-11-16 17:21:05 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
  *  - Supabase užklausos – VISADA per tinklą (nekaišioja cache)
  */
 
-const VERSION = 'v55-2025-11-15';
+const VERSION = 'v56-2025-11-15';
 const BASE = '/timurita';
 
 const CACHE_STATIC = `timurita-static-${VERSION}`;
